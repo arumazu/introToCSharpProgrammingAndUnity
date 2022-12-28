c698bfecf1438edf0a10e1a6b19259bb23d6121d
Arumazu <arumazu@users.noreply.github.com>
2022-12-28 15:30:08 +0500

    Exercise 9

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise9
+{
+    /// <summary>
+    /// Exercise 9 solution 
+    /// </summary>
+
+    internal class Program
+    {
+        /// <summary>
+        /// Demonstrates using custom classes
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and tell the deck to print itself
+            Deck deck = new Deck();
+            deck.Print();
+
+            // tell the deck to shuffle and print itself
+            deck.Shuffle();
+            deck.Print();
+
+            // take the top card from the deck and print the card rank and suit
+            Card card0 = deck.TakeTopCard();
+            Console.WriteLine("First card: " + card0.Rank + " of " + card0.Suit);
+
+            // take the top card from the deck and print the card rank and suit
+            Card card1 = deck.TakeTopCard();
+            Console.WriteLine("Second card: " + card1.Rank + " of " + card1.Suit);
+        }
+    }
+}
