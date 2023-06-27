18dcd9e1c8159ed9947fde9abe8e48389a003481
Arumazu <arumazu@users.noreply.github.com>
2023-06-27 22:29:27 +0500

    deleted:    Exercise1/bin/Debug/net7.0/Exercise1.deps.json 	deleted:    Exercise1/bin/Debug/net7.0/Exercise1.dll 	deleted:    Exercise1/bin/Debug/net7.0/Exercise1.pdb 	deleted:    Exercise1/bin/Debug/net7.0/Exercise1.runtimeconfig.json 	deleted:    Exercise1/obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.AssemblyInfo.cs 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.AssemblyInfoInputs.cache 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.GeneratedMSBuildEditorConfig.editorconfig 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.GlobalUsings.g.cs 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.assets.cache 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.csproj.AssemblyReference.cache 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.csproj.CoreCompileInputs.cache 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.csproj.FileListAbsolute.txt 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.dll 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.genruntimeconfig.cache 	deleted:    Exercise1/obj/Debug/net7.0/Exercise1.pdb 	deleted:    Exercise1/obj/Debug/net7.0/ref/Exercise1.dll 	deleted:    Exercise1/obj/Debug/net7.0/refint/Exercise1.dll 	deleted:    Exercise1/obj/Exercise1.csproj.nuget.dgspec.json 	deleted:    Exercise1/obj/Exercise1.csproj.nuget.g.props 	deleted:    Exercise1/obj/Exercise1.csproj.nuget.g.targets 	deleted:    Exercise1/obj/project.assets.json 	deleted:    Exercise1/obj/project.nuget.cache 	new file:   Exercise11 Unity/GreenDinoScale.cs 	new file:   Exercise12 Unity/Mover.cs

---
+++ Exercise11 Unity/GreenDinoScale.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenDinoScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		// quadruple width and height
+		Vector3 newScale = transform.localScale;
+		
+		newScale.x *= 4;
+		newScale.y *= 4;
+		
+		transform.localScale = newScale;
+		
+    }
+
+}
---
+++ Exercise12 Unity/Mover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		// get the game object moving
+		
+		GetComponent<Rigidbody2D>().AddForce(
+
+			new Vector2(0, 5),
+
+			ForceMode2D.Impulse);
+        
+    }
+
+}
